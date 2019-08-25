00d016e37b6083f5369c776a31a7917885fed0d9
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-08-25 19:13:18 +0200

    Before saving to file

--- apa.js
+++ apa.js
@@ -1,7 +1,10 @@
-const arr = [{ key: 1 }, { key: 2 }, { key: 3 }]
+// const arr = [{ key: 1 }, { key: 2 }, { key: 3 }]
 
-const results = arr.map(async (obj) => { return obj.key })
+// const results = arr.map(async (obj) => { return obj.key })
 
-Promise
-  .all(results)
-  .then(console.log)
+// Promise
+//   .all(results)
+//   .then(console.log)
+
+const arr = [[1, 2], [3, 4], [5, 6]]
+console.log(arr.flat()) // [1,2,3,4,5,6]
--- index.js
+++ index.js
@@ -27,4 +27,46 @@ const parseLinks = async htmlString => {
   }
 }
 
-//getData(getKidUrl('Albert')).then(parseLinks).then(console.log)
+// getData(getKidUrl('Albert')).then(parseLinks).then(console.log)
+
+const getQuoteHtmlString = async link => getData(link)
+
+const parseQuoteDate = kidInfoString => kidInfoString.split(';')[0].split(' ')[1]
+const parseKidName = kidInfoString => kidInfoString.split(' av ')[1].split('\n')[0]
+const parseAgeAtQuote = kidInfoString => kidInfoString.split('\n')[1].trim()
+
+const parseQuote = async htmlString => {
+  const $ = await cheerio.load(htmlString)
+  const quoteText = $('blockquote > p').text().trim()
+  const kidInfo = $('#contentMain > div > p:nth-child(2)').text().trim()
+
+  return {
+    quoteText: quoteText,
+    quoteDate: parseQuoteDate(kidInfo),
+    kidName: parseKidName(kidInfo),
+    ageAtQuote: parseAgeAtQuote(kidInfo)
+  }
+}
+// getQuoteHtmlString('http://abbesays.apphb.com/Quotes/77').then(parseQuote).then(console.log)
+
+const getQuotes = async links => {
+  const parsedQuotesPromises =
+    links.map(async link => {
+      console.log(`${link} being processed`)
+      const quoteHtml = await getQuoteHtmlString(link)
+      return parseQuote(quoteHtml)
+    })
+
+  return Promise.all(parsedQuotesPromises)
+}
+
+const kids = ['Albert', 'Arvid', 'Gustav']
+
+Promise.all(
+  kids.map(async kid =>
+    getData(getKidUrl(kid))
+      .then(parseLinks)
+      .then(getQuotes)
+  ))
+  .then(kidQuotes => kidQuotes.flat())
+  .then(console.table)
